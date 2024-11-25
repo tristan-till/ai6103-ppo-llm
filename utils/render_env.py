3240fb92ba6f1df006953d476356704526d17a94
Cyril Gliner <cgliner@outlook.com>
2024-11-25 21:29:34 +0800

    render env with fake env

--- utils/render_env.py
+++ utils/render_env.py
@@ -74,7 +74,7 @@ def place_goal(background, goal):
     place_img("sprites/goal.png", background, goal)
     return background
 
-def place_agent(background, state_str, size, holes):
+def place_agent(background, state_str, size, holes, mode):
     state, orientation = state_str.split("_")
     state = int(state)
     orientation_str = orientation_map[orientation]
@@ -103,7 +103,7 @@ def _render_state(grid, state_str, mode):
     background = place_lakes(background, holes)
     background = place_stool(background, start)
     background = place_goal(background, goal)
-    background = place_agent(background, state_str, size, holes)
+    background = place_agent(background, state_str, size, holes, mode)
     return background
 
 def base64_from_state_arr(state_arr):
@@ -121,6 +121,89 @@ def render_img_and_embedding(grid, state_str, mode):
     embedding = sentence_transformer.encode(llama)
     state = np.append(render.flatten(), embedding)
     return state
+
+def render_img_and_embedding_fake(grid, state_str, mode, proba_hallu):
+    render = render_arr(grid, state_str, mode)
+    base64_img = base64_from_state_arr(render)
+    # llava = req.get_llava(base64_img)
+    # llama = req.get_llama(llava)
+    llava = get_custom_state_str(grid, state_str)
+    # llava = get_fake_llava(grid, state_str, proba_hallu)
+    embedding = sentence_transformer.encode(llava)
+    state = np.append(render.flatten(), embedding)
+    return render, llava, state
+
+def get_custom_state_str(grid, state_str):
+    state, _ = state_str.split("_")
+    state = int(state)
+    agent_tile = int(state % 4), int(state / 4)
+    _, _, _, holes = parse_grid(grid)
+    agent_x, agent_y = agent_tile
+    embedd_str = "Agent "
+    embedd_str += "top " if agent_y < 2 else "bottom "
+    embedd_str += "left, " if agent_x < 2 else "right, "
+
+    for hole in holes:
+        hole_x, hole_y = hole
+        if agent_x == hole_x - 1 and agent_y == hole_y:
+            embedd_str += "lake right of agent, "
+        elif agent_x == hole_x + 1 and agent_y == hole_y:
+            embedd_str += "lake left of agent, "
+        elif agent_y == hole_y - 1 and agent_x == hole_x:
+            embedd_str += "lake below agent, "
+        elif agent_y == hole_x + 1 and agent_x == hole_x:
+            embedd_str += "lake above agent, "
+
+    embedd_str += "present bottom right."
+    return embedd_str
+
+def get_fake_llava(grid, state_str, proba_hallu = 0):
+
+    pos1 = ['top', 'middle', 'middle','bottom']
+    pos2 = ['left', 'middle', 'middle', 'right']
+    txt = ''
+
+    size, start, goal, holes = parse_grid(grid)
+
+    state, _ = state_str.split("_")
+    state = int(state)
+    agt = int(state % size), int(state / size)
+
+    min_val = 0
+    max_val = size - 1
+
+    goal = perturb_pos(proba_hallu, goal, max_val, min_val)
+    agt  = perturb_pos(proba_hallu, agt, max_val, min_val)
+
+
+    for idx, hole in enumerate(holes):
+        holes[idx] = perturb_pos(proba_hallu, holes[idx],max_val, min_val)
+    
+
+    agt_str = f'Character is {pos1[agt[1]]} {pos2[agt[0]]}. '
+    txt += agt_str
+
+    for hole in holes:
+        txt += f'Lake at {pos1[hole[1]]} {pos2[hole[0]]}. '
+
+    gift_str = f'Gift is {pos1[goal[1]]} {pos2[goal[0]]}.'
+    txt += gift_str
+
+    return txt
+
+def perturb_pos(proba, pos, max_val, min_val):
+    
+    new_pose = np.zeros_like(pos)
+
+    n = len(pos)
+
+    for i in range(n):
+        if np.random.rand() < proba:
+            new_pose[i] = max(min_val,(min(max_val, pos[i] + np.random.choice([-1, 1]))))
+        else:
+            new_pose[i] = pos[i]
+
+    return new_pose
 
 def render_embedding(grid, state_str, mode):
     render = render_arr(grid, state_str, mode)
